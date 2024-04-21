3135a028024404a9e271d0129f14edd69dbcc9bc
Lib3Rt9 <phamgiaphuc011709@gmail.com>
2024-04-21 15:43:01 +0700

    + update a liitle labwork 2

--- labwork2/labwork2.py
+++ labwork2/labwork2.py
@@ -1,3 +1,5 @@
+import csv
+
 def means(values):
     return sum(values) / float(len(values))
 
@@ -21,7 +23,7 @@ def coefficients(data):
 
     b1 = covariance(x_values, y_values, x_mean, y_mean) / variance(x_values, x_mean)
     b0 = y_mean - b1 * x_mean
-    
+
     return [b0, b1]
 
 def linear_regression(train, test):
@@ -35,3 +37,23 @@ def linear_regression(train, test):
         predictions.append(yhat)
 
     return predictions
+
+
+def testing():
+    with open('winequality-white.csv', 'r') as f:
+        winewhites = list(csv.reader(f, delimiter=';'))
+
+    for i in range(len(winewhites[0])):
+        for row in winewhites:
+            row[i] = float(row[i].strip())
+
+    train_size = int(0.8 * len(winewhites))
+    train, test = winewhites[:train_size], winewhites[train_size:]
+
+    x = [row[0] for row in train]
+    y = [row[1] for row in train]
+
+    train = list(zip(x, y))
+    test = list(zip(x, y))
+
+    predictions = linear_regression(train, test)
